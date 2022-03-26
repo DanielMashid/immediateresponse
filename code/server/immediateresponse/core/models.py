e9a18dfa654bbc0df70a83f4ea9dd55d34dbf496
DorNakash <dorx33@gmail.com>
2022-03-26 12:51:35 +0300

    serializers & model with fk

--- code/server/immediateresponse/core/models.py
+++ code/server/immediateresponse/core/models.py
@@ -1,28 +1,25 @@
 from django.db import models
 
-
-class User(models.Model):
-    user_name = models.CharField(max_length=100)
+class Organization(models.Model):
     organization_number = models.IntegerField()
-    avatar = models.CharField(max_length=100)  # temporary
-    phone_number = models.IntegerField()
-    is_supervisor = models.BooleanField()
-
+    
 
 class AvatarImage(models.Model):
     avatar_image = models.CharField(max_length=100)
 
-
-class Organization(models.Model):
-    organization_number = models.IntegerField()
-
+class User(models.Model):
+    user_name = models.CharField(max_length=100)
+    organization_number = models.ForeignKey(Organization,on_delete=models.CASCADE,related_name="orgumber")
+    avatar = models.ForeignKey(AvatarImage, on_delete=models.CASCADE,related_name="avatarimg")  # temporary
+    phone_number = models.IntegerField()
+    is_supervisor = models.BooleanField()
 
 class EventImage(models.Model):
     event_image = models.CharField(max_length=100)
 
-
 class Event(models.Model):
-    reported_user = models.CharField(max_length=100)  # temporary
+    reported_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reportuser")  # temporary
     longitude = models.CharField(max_length=100)
     latitude = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
+
