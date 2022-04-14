fab1a2932a8c765cda10c1e0d369d482f85f4122
DorNakash <dorx33@gmail.com>
2022-04-14 16:16:08 +0300

    quick fix server urls

--- code/server/immediateresponse/core/urls.py
+++ code/server/immediateresponse/core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns ={
+urlpatterns =[
     path('post_incident/',views.post_incident),
     path('get_incident/',views.get_incident),
 
-}
+]
--- code/server/immediateresponse/immediateresponse/urls.py
+++ code/server/immediateresponse/immediateresponse/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from core import urls as immediateresponse_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(immediateresponse_urls)),
+    path('',include('core.urls')),
 ]
