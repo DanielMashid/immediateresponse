81542f5cdf0f983957b41ba4d46e854eee31137f
DanielMashid <danielmashid1@gmail.com>
2022-04-18 12:01:47 +0300

    Worked on location of event. At the moment it prints the location to the console. On the next commit should send the location by mail.

--- code/client/immediateresponse/src/components/IncidentsScreenBody/IncidentsScreenGrid.js
+++ code/client/immediateresponse/src/components/IncidentsScreenBody/IncidentsScreenGrid.js
@@ -3,22 +3,57 @@ import './IncidentsScreenGrid.css'
 import {Button, ButtonGroup} from "@material-ui/core";
 
 function SimpleText() {
+
+    const [latitude, setLatitude] = React.useState(null);
+    const [longitude, setLongitude] = React.useState(null);
+
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getCoordinates, handlerLocationError);
+        } else {
+            alert("Geolocation is not supported by this browser.")
+        }
+    }
+    const getCoordinates = (position) => {
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+        printState()
+    }
+    const handlerLocationError = (error) => {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                alert("User denied the request for Geolocation.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("Location information is unavailable.");
+                break;
+            case error.TIMEOUT:
+                alert("The request to get user location timed out.");
+                break;
+            default:
+                alert("An unknown error occurred.");
+        }
+    }
+    const printState = () => {
+        console.log(longitude, latitude)
+    }
+
     return (
         <section>
-        <div className="mainText3">
-            <ButtonGroup className="mainText5">
-                <Button>שפך</Button>
-                <Button>אירוע ביטחוני</Button>
-                <Button>בטיחות פגיעה באדם</Button>
-            </ButtonGroup>
-        </div>
+            <div className="mainText3">
+                <ButtonGroup className="mainText5">
+                    <Button id={'estuary_incident'} onClick={getLocation}>שפך</Button>
+                    <Button id={'security_incident'} onClick={getLocation}>אירוע ביטחוני</Button>
+                    <Button id={'safety_person_incident'} onClick={getLocation}>בטיחות פגיעה באדם</Button>
+                </ButtonGroup>
+            </div>
             <div className="mainText4">
-            <ButtonGroup className="mainText5">
-                <Button>שריפה</Button>
-                <Button>חומר מעשן</Button>
-                <Button>בטיחות פגיעה ברכוש</Button>
-            </ButtonGroup>
-        </div>
+                <ButtonGroup className="mainText5">
+                    <Button id={'fire_incident'} onClick={getLocation}>שריפה</Button>
+                    <Button id={'materials_incident'} onClick={getLocation}>חומר מעשן</Button>
+                    <Button id={'safety_property_incident'} onClick={getLocation}>בטיחות פגיעה ברכוש</Button>
+                </ButtonGroup>
+            </div>
         </section>
     );
 }
