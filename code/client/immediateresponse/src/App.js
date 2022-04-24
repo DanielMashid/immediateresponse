3ef23b673a9e59ae2cf4dea61d80481d262f6ec3
DorNakash <dorx33@gmail.com>
2022-04-24 16:16:23 +0300

    Changed the front to be with state on paging

--- code/client/immediateresponse/src/App.js
+++ code/client/immediateresponse/src/App.js
@@ -1,27 +1,24 @@
 //import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login  from './screens/LoginScreen/loginScreen';
-import Sos from './screens/SosScreen/sosScreen'
-import Incidents from './screens/IncidentsScreen/IncidentsScreen'
-import Chat from './screens/ChatScreen/ChatScreen'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./screens/LoginScreen/loginScreen";
+import Sos from "./screens/SosScreen/sosScreen";
+import Incidents from "./screens/IncidentsScreen/IncidentsScreen";
+import Chat from "./screens/ChatScreen/chatScreen";
 
 function App() {
-    return (
-
-     <div>
-         <Router>
-            <Routes>
-              <Route path="/" element={<Login/>}/>
-              <Route path="/login" element={<Login/>}/>
-              <Route path="/sos" element={<Sos />}/>
-              <Route path="/incidents" element={<Incidents />} />
-              <Route path="/chat" element={<Chat />}/>
-             </Routes>
-         </Router>      
-
-     </div>
-
-    );
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sos" element={<Sos />} />
+          <Route path="/incidents" element={<Incidents />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- code/client/immediateresponse/src/components/IncidentsScreenBody/IncidentsScreenGrid.js
+++ code/client/immediateresponse/src/components/IncidentsScreenBody/IncidentsScreenGrid.js
@@ -1,27 +1,59 @@
-import React from "react";
-import './IncidentsScreenGrid.css'
-import {Button, ButtonGroup} from "@material-ui/core";
+import React, { useContext } from "react";
+import "./IncidentsScreenGrid.css";
+import { Button, ButtonGroup } from "@material-ui/core";
+import { Link, useNavigate } from "react-router-dom";
+
+import IncidentContext from "../../context/IncidentContext";
 
 function SimpleText() {
-    return (
-        <section>
-        <div className="mainText3">
-            <ButtonGroup className="mainText5">
-                <Button>שפך</Button>
-                <Button>אירוע ביטחוני</Button>
-                <Button>בטיחות פגיעה באדם</Button>
-            </ButtonGroup>
-        </div>
-            <div className="mainText4">
-            <ButtonGroup className="mainText5">
-                <Button>שריפה</Button>
-                <Button>חומר מעשן</Button>
-                <Button>בטיחות פגיעה ברכוש</Button>
-            </ButtonGroup>
-        </div>
-        </section>
-    );
+  const incidentContext = useContext(IncidentContext);
+
+  const reportIncidentOnClick = (e) => {
+    incidentContext.incident = e.currentTarget.id;
+    console.log(incidentContext);
+  };
+  return (
+    <div>
+      <div className="mainText3">
+        <ButtonGroup className="mainText5">
+          <Link to="/chat">
+            <Button id={"שפך"} onClick={reportIncidentOnClick}>
+              שפך
+            </Button>
+          </Link>
+          <Link to="/chat">
+            <Button id={"אירוע ביטחוני"} onClick={reportIncidentOnClick}>
+              אירוע ביטחוני
+            </Button>
+          </Link>
+          <Link to="/chat">
+            <Button id={"בטיחות פגיעה באדם"} onClick={reportIncidentOnClick}>
+              בטיחות פגיעה באדם
+            </Button>
+          </Link>
+        </ButtonGroup>
+      </div>
+      <div className="mainText4">
+        <ButtonGroup className="mainText5">
+          <Link to="/chat">
+            <Button id={"שריפה"} onClick={reportIncidentOnClick}>
+              שריפה
+            </Button>
+          </Link>
+          <Link to="/chat">
+            <Button id={"חומר מעשן"} onClick={reportIncidentOnClick}>
+              חומר מעשן
+            </Button>
+          </Link>
+          <Link to="/chat">
+            <Button id={"בטיחות פגיעה ברכוש"} onClick={reportIncidentOnClick}>
+              בטיחות פגיעה ברכוש
+            </Button>
+          </Link>
+        </ButtonGroup>
+      </div>
+    </div>
+  );
 }
 
-export default SimpleText
-
+export default SimpleText;
