a51901229dd15812edaaad3533417e16526d251d
DorNakash <dorx33@gmail.com>
2022-05-09 13:27:52 +0300

    First step forward

--- code/server/immediateresponse/chat/urls.py
+++ code/server/immediateresponse/chat/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    # path('', views.index, name='index'),
+    # path('<str:room_name>/', views.room, name='room'),
+    path('get-chatroom/', views.get_chatroom, name='get-chatroom'),
+    path('new-chatroom/', views.new_chatroom, name='new-chatroom'),
 ]
--- code/server/immediateresponse/core/urls.py
+++ code/server/immediateresponse/core/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('post_incident/', views.post_incident),
     path('get_incident/', views.get_incident),
-    path('api/report_incident_by_mail/', views.report_incident_by_mail)
+    path('api/report_incident_by_mail/', views.report_incident_by_mail),
 ]
