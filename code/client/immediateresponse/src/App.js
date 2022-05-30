97470c08b6948630e622c32066a3b717405e64d8
DorNakash <dorx33@gmail.com>
2022-05-30 14:50:02 +0300

    Hash router

--- code/client/immediateresponse/src/App.js
+++ code/client/immediateresponse/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./screens/LoginScreen/loginScreen";
 import Sos from "./screens/SosScreen/sosScreen";
 import Incidents from "./screens/IncidentsScreen/IncidentsScreen";
